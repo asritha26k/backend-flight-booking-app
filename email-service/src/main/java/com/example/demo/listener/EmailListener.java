1a11642a8a55b75e6fe7ecf4b7c747c162bab554
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-12-20 22:59:44 +0530

    created mailtrap email service

--- email-service/src/main/java/com/example/demo/listener/EmailListener.java
+++ email-service/src/main/java/com/example/demo/listener/EmailListener.java
@@ -1,17 +1,24 @@
 package com.example.demo.listener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.service.EmailService;
+import com.example.event.TicketBookedEvent;
+
 @Service
 public class EmailListener {
-	private static final Logger logger = LoggerFactory.getLogger(EmailListener.class);
 
-	@KafkaListener(topics = "${kafka.topic.ticket-booked}", groupId = "${kafka.group.email}")
-	public void listen(String message) {
-		logger.info("📩 Email-service received: {}", message);
+    @Autowired
+    private EmailService emailService;
+    @KafkaListener(topics = "${kafka.topic.ticket-booked}", groupId = "email-group")
+    public void listen(TicketBookedEvent event) {
 
-	}
+        emailService.sendBookingEmail(
+            event.getEmail(),
+            "Your ticket is booked.\nPNR: " + event.getPnr()
+        );
+    }
+
 }
