9935d63ec5d9ff25cfaa39425b996ea564198a60
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-12-21 15:17:30 +0530

    working in local and docker

--- email-service/src/main/java/com/example/demo/listener/EmailListener.java
+++ email-service/src/main/java/com/example/demo/listener/EmailListener.java
@@ -14,11 +14,23 @@ public class EmailListener {
     private EmailService emailService;
     @KafkaListener(topics = "${kafka.topic.ticket-booked}", groupId = "email-group")
     public void listen(TicketBookedEvent event) {
+        System.out.println("===== EMAIL SERVICE KAFKA LISTENER =====");
+        System.out.println("Event received from Kafka");
+        System.out.println("Email: " + event.getEmail());
+        System.out.println("PNR  : " + event.getPnr());
+        System.out.println("=======================================");
+        try {
+            emailService.sendBookingEmail(
+                    event.getEmail(),
+                    "Your ticket is booked.\nPNR: " + event.getPnr()
+            );
 
-        emailService.sendBookingEmail(
-            event.getEmail(),
-            "Your ticket is booked.\nPNR: " + event.getPnr()
-        );
+            System.out.println("Email sent successfully");
+
+        } catch (Exception e) {
+            System.out.println("Email sending failed");
+            e.printStackTrace();
+        }
     }
 
 }
